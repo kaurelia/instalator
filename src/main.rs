4c75092705ca0c153577e2c141edcb03949bd26c
Aurelia Korbela <aurelka2000@gmail.com>
2024-02-12 22:00:32 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,84 @@
+use colorful::{Color, Colorful};
 use core::panic;
-use std::{collections::HashMap, io::Read};
 use sevenz_rust::*;
-use steamlocate::{SteamDir, SteamApp};
-use colorful::{Color,Colorful};
-use std::io::{stdin, stdout};
 use std::io::prelude::*;
-
+use std::io::{stdin, stdout};
+use std::{collections::HashMap, io::Read};
+use steamlocate::{SteamApp, SteamDir};
 
 const ASSETTO_CORSA_APP_ID: u32 = 244210;
 
 fn pause() {
     let mut stdin = stdin();
     let mut stdout = stdout();
-    write!(stdout, "Wciśnij Enter, żeby kontynuować...").unwrap();
+    write!(stdout, "Wciśnij Enter, żeby opuścić kreator instalacji...").unwrap();
     stdout.flush().unwrap();
     let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
 fn main() {
     let addon_folder_name: &'static str = "123";
-    println!("{}", "Szukanie aplikacji steam na komputerze...".color(Color::Cyan));
+    println!(
+        "{}",
+        "Szukanie aplikacji steam na komputerze...".color(Color::Cyan)
+    );
     let steam_directory_option = SteamDir::locate();
-    if steam_directory_option.is_none(){
-        println!("{}", "Nie znaleziono aplikacji steam na twoim komputerze".color(Color::Red));
+    if steam_directory_option.is_none() {
+        println!(
+            "{}",
+            "Nie znaleziono aplikacji steam na komputerze".color(Color::Red)
+        );
         pause();
         panic!("");
     }
     let mut steam_directory = steam_directory_option.unwrap();
+    println!(
+        "{}",
+        "Szukanie gry Assetto Corsa na komputerze...".color(Color::Cyan)
+    );
     let apps: &HashMap<u32, Option<SteamApp>> = steam_directory.apps();
     let assetto_corsa = apps.get(&ASSETTO_CORSA_APP_ID);
     if assetto_corsa.is_none() {
-        panic!("Asseto corsa not found");
+        println!(
+            "{}",
+            "Nie znaleziono gry Assetto Corsa na komputerze".color(Color::Red)
+        );
+        pause();
+        panic!("");
     }
     let assetto_corsa = assetto_corsa.unwrap();
     if assetto_corsa.is_none() {
-        panic!("Asseto corsa not found");
+        println!(
+            "{}",
+            "Nie znaleziono gry Assetto Corsa na twoim komputerze".color(Color::Red)
+        );
+        pause();
+        panic!("");
     }
     let assetto_corsa = assetto_corsa.as_ref().unwrap();
-    let destination_path = (&assetto_corsa.path).clone().into_os_string().into_string().unwrap();
+    let destination_path = (&assetto_corsa.path)
+        .clone()
+        .into_os_string()
+        .into_string()
+        .unwrap();
     let destination_path = format!("{destination_path}\\content\\cars\\{addon_folder_name}");
-    println!("{:?}", destination_path);
-    let result = decompress_file(format!("{addon_folder_name}.7z"), destination_path);
-    println!("{result:#?}");
-    //println!("{:?}", apps);
-    //println!("Hello, world!");
+    println!("{}", "Instalacja modyfikacji...".color(Color::Cyan));
+    let decompress_result = decompress_file(format!("{addon_folder_name}.7z"), destination_path);
+    match decompress_result {
+        Ok(_) => {
+            println!(
+                "{}",
+                "Zainstalowano modyfikacje pomyślnie".color(Color::Green)
+            );
+            pause();
+        }
+        Err(_) => {
+            println!(
+                "{}",
+                "Wystąpił nieoczekiwany błąd podczas instalacji".color(Color::Red)
+            );
+            pause();
+            panic!("");
+        }
+    }
 }
