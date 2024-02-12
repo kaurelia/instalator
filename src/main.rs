bcaf73c8e2ce6c0fd79011b5e69db133116cf366
Aurelia Korbela <aurelka2000@gmail.com>
2024-02-12 20:55:36 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let addon_folder_name: &'static str = "123";
     let mut steam_directory = SteamDir::locate().unwrap();
     let apps: &HashMap<u32, Option<SteamApp>> = steam_directory.apps();
-    //
     let assetto_corsa = apps.get(&ASSETTO_CORSA_APP_ID);
     if assetto_corsa.is_none() {
         panic!("Asseto corsa not found");
@@ -18,9 +17,11 @@ fn main() {
         panic!("Asseto corsa not found");
     }
     let assetto_corsa = assetto_corsa.as_ref().unwrap();
-    let destination_path = (&assetto_corsa.path).clone();
+    let destination_path = (&assetto_corsa.path).clone().into_os_string().into_string().unwrap();
+    let destination_path = format!("{destination_path}\\content\\cars\\{addon_folder_name}");
     println!("{:?}", destination_path);
-    let _ = decompress_file(format!("{addon_folder_name}.7z"), "{addon_folder_name}");
+    let result = decompress_file(format!("{addon_folder_name}.7z"), destination_path);
+    println!("{result:#?}");
     //println!("{:?}", apps);
     //println!("Hello, world!");
 }
